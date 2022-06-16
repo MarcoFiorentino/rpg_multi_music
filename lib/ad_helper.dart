7d37485ab86cb494da180ff09461b05c38c5a556
Marco <mfiore89@gmail.com>
2022-06-16 23:39:50 +0200

    Aggiornati id pubblicità

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -2,7 +2,7 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class AdHelper {
   static BannerAd homeBanner = BannerAd(
-    adUnitId: 'ca-app-pub-3940256099942544/6300978111',
+    adUnitId: 'ca-app-pub-5556746391287393/8462929248',
     size: AdSize.banner,
     request: AdRequest(),
     listener: BannerAdListener(),
